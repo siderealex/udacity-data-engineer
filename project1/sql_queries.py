f48cbf437f72cc5912fa2a930aadd07c66f692e4
Al Vaccaro <alexandervaccaro@gmail.com>
2020-03-18 12:14:32 -0700

    Add CREATE/INSERT queries

--- project1/sql_queries.py
+++ project1/sql_queries.py
@@ -1,44 +1,104 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE songplays"
+user_table_drop = "DROP TABLE users"
+song_table_drop = "DROP TABLE songs"
+artist_table_drop = "DROP TABLE artists"
+time_table_drop = "DROP TABLE time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TABLE songplays (
+    songplay_id int PRIMARY KEY NOT NULL,
+    start_time time,
+    user_id int,
+    level int,
+    song_id int,
+    artist_id int,
+    session_id int,
+    location varchar, user_agent varchar
+)
 """)
 
 user_table_create = ("""
+CREATE TABLE users (
+    user_id int PRIMARY KEY NOT NULL,
+    first_name varchar,
+    last_name varchar,
+    gender varchar,
+    level int
+)
 """)
 
 song_table_create = ("""
+CREATE TABLE songs (
+    song_id int PRIMARY KEY NOT NULL,
+    title varchar,
+    artist_id int,
+    year int,
+    duration time
+)
 """)
 
 artist_table_create = ("""
+CREATE TABLE artists (
+    artist_id int PRIMARY KEY NOT NULL,
+    name varchar,
+    location varchar,
+    latitude numerical,
+    longitude numerical
+)
 """)
 
 time_table_create = ("""
+CREATE TABLE time (
+    start_time time,
+    hour int,
+    day int,
+    week int,
+    month int,
+    year int,
+    weekday varchar
+)
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+INSERT INTO songplays (
+    songplay_id,
+    start_time,
+    user_id,
+    level,
+    song_id,
+    artist_id,
+    sesion_id,
+    location,
+    user_agent
+)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+VALUES (%s, %s, %s, %s, %s)
 """)
 
 song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+VALUES (%s, %s, %s, %s, %s)
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+VALUES (%s, %s, %s, %s, %s)
 """)
 
 
 time_table_insert = ("""
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+VALUES (%s, %s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
